a1a66bf7a1e201bed5b3250a37afbc15a5a5f7e4
PHMor <163937840+PHMor@users.noreply.github.com>
2024-03-20 16:51:31 -0300

    ATV2.cpp

--- ATV2.cpp
+++ ATV2.cpp
@@ -2,18 +2,22 @@ using namespace std;
 #include <iostream>
 #include <stdlib.h>
 int main () {
-    int nq;
-    const int vq = 150;
+    int nqp;
+    int nqg;
+    const int vqg = 150;
+    const int vqp = 100;
     int c;
     double tx = 0.06;
     double imp;
     double ct;
-    cout << "Quantos quartos serao limpos?";
-    cin >> nq;
+    cout << "Quantos quartos pequenos serao limpos?";
+    cin >> nqp;
+     cout << "Quantos quartos grandes serao limpos?";
+    cin >> nqg;
     system ("cls");
-    c = nq*vq;
+    c = (nqp*vqp)+(nqg*vqg);
     imp = c*tx;
     ct = c+imp;
-    cout <<"Orcamento para servico de limpeza de carpetes\nNumero de quartos: "<<nq<<"\nValor por quarto: "<<vq<<"\nCusto: "<<c<<"\nImposto: "<<imp<<"\n===============================================\nCusto total: "<<ct;
+    cout <<"Orcamento para servico de limpeza de carpetes\nNumero de quartos pequenos: "<<nqp<<"\nValor por quarto pequeno: "<<vqp<<"\nNumero de quartos grandes: "<<nqg<<"\nValor por quarto grande: "<<vqg<<"\nCusto: "<<c<<"\nImposto: "<<imp<<"\n===============================================\nCusto total: "<<ct<<"\nOrçamento valido por 30 dias";
     return 0;
 }
