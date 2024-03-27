072585a89d2b21603e8636cfea64032773a28003
pHmor <163937840+PHMor@users.noreply.github.com>
2024-03-27 20:03:08 -0300

    Add files via upload

---
+++ 7.2.3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float m;
+    cout<<"Digite uma distancia em metros:\n";
+    cin>>m;
+    cout<<"dc: "<<m*10<<"dm"<<endl<<"cm: "<<m*100<<"cm"<<endl<<"mm: "<<m*1000<<"mm";
+    return 0;
+}
